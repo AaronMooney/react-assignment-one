4f6ca23692222e7554966837911b2cf135a46439
Aaron Mooney <aaron.mooney@errigal.com>
2020-03-05 18:30:25 +0000

    Initial commit - Movies lab starting point

---
+++ .storybook/config.js
@@ -0,0 +1,8 @@
+import { configure, addDecorator } from '@storybook/react';
+
+
+function loadStories() {
+  require('../stories/index');
+}
+
+configure(loadStories, module);
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import HomePage from "./pages/homepage";
+import MoviePage from './pages/movieDetailsPage';
+import FavoriteMoviesPage from './pages/favoriteMoviesPage';
+import MovieReviewPage from './pages/movieReviewPage';
+import "./globals/fontawesome";
+import SiteHeader from './components/siteHeader';
+import MoviesContextProvider from "./contexts/moviesContext";
+import GenresContextProvider from "./contexts/genresContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div className="jumbotron">
+      <SiteHeader/>
+      <div className="container-fluid">
+          <MoviesContextProvider>
+          <GenresContextProvider>
+              <Switch>
+                <Route exact path="/reviews/form" component={AddMovieReviewPage} />
+                <Route path="/reviews/:id" component={MovieReviewPage} />
+                <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+                <Route path="/movies/:id" component={MoviePage} />
+                <Route path="/" component={HomePage} />
+                <Redirect from="*" to="/" />
+              </Switch>
+            </GenresContextProvider>
+          </MoviesContextProvider>
+        </div>
+      </div>
+  </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));

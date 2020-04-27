32f4646d26df41f63dfbb86792c18aca1931118b
Aaron Mooney <aaron.mooney@errigal.com>
2020-04-27 22:01:19 +0100

    fixed button spacing in favorites

--- src/components/buttons/addTvShowReview.js
+++ src/components/buttons/addTvShowReview.js
@@ -5,7 +5,7 @@ const TvShowReviewButton = ({ tvShow }) => {
   return (
     <Link
       className="btn w-100"
-      style={{backgroundColor:"#0174AD",color:"white"}}
+      style={{backgroundColor:"#0174AD",color:"white",margin:"10px 0"}}
       to={{
         pathname: `/tvShows/reviews/form`,
         state: {
